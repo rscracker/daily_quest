1a945c7f5259e9bc829a9fefae4a9df6afab468c
park <rscracker@naver.com>
2024-04-18 22:54:33 +0900

    refactor : env파일 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,16 @@ import 'package:daily_quest/app/app.dart';
 import 'package:daily_quest/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  KakaoSdk.init(nativeAppKey: '0700e6112708fb886880a896356af552');
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_API_KEY']);
   runApp(const App());
 }
